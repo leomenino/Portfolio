13aef3fadd387ab0d22d4d6155ecb64c5ffa8c1e
leomenino <leonelmenino@gmail.com>
2024-06-07 14:49:02 +0100

    burger changes

--- scripts/main.js
+++ scripts/main.js
@@ -3,10 +3,10 @@ const navSlide = () => {
     const burger = document.querySelector(".burger")
     const navLists = document.querySelector("nav")
 
-    burger.addEventListener("click", () => {
+    
         navLists.classList.toggle("nav-active");
         burger.classList.toggle("toggle-burger");
-    });
+    
 };
 // Clear form before unload
 window.onbeforeunload = () => {
